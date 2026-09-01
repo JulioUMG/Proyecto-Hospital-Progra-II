4e09c28b6d5c1ab3eee9e1ef49826a89daf68c02
JulioUMG <jcordonr@miumg.edu.gt>
2026-08-31 22:57:30 -0600

    Carga de Proyecto Inicial SpringBoot

---
+++ src/main/java/com/hotel404/Hotel404Application.java
@@ -0,0 +1,13 @@
+package com.hotel404;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hotel404Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hotel404Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotel404/hotel404/Hotel404ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel404.hotel404;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hotel404ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
